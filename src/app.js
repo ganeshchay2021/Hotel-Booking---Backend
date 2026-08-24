6e480589239879871bda5572294767197a392806
Ganesh Chaudhary <ganeshchay2021@gmail.com>
2026-08-24 15:20:33 +0545

    Fixed Some Files

--- src/app.js
+++ src/app.js
@@ -1,7 +1,6 @@
 import express from "express";
 import cors from 'cors';
 import { clerkMiddleware } from "@clerk/express";
-import Router from "./Routes/clerkwebhook.routes";
 import clerkWebHook from "./Controllers/clerkwebhook";
 
 const app = express();
