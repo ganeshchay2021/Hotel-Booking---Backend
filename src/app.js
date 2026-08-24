db147f3e2a8f5e63fe7a946792cbea020947717d
Ganesh Chaudhary <ganeshchay2021@gmail.com>
2026-08-24 16:38:08 +0545

    app file updated

--- src/app.js
+++ src/app.js
@@ -14,10 +14,18 @@ app.use(cors())     //enable cross origin resources sharing
 app.use(express.json());
 app.use(clerkMiddleware())
 
+
+app.post(
+    "/api/webhooks/clerk",
+    express.raw({ type: "application/json" }),
+    clerkWebHook
+);
+
+
 /*
 *   API to listen clerk webhooks
 */
-app.use("/api/clerk", clerkWebHook)
+// app.use("/api/clerk", clerkWebHook)
 
 
 app.get('/', (req, res) => {
