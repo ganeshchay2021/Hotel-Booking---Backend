5ac7ace62b7d0a0a9bc4a0d4e8b377bcb505c7fe
Ganesh Chaudhary <ganeshchay2021@gmail.com>
2026-08-25 13:28:37 +0545

    Configs file added

--- src/Config/config.js
+++ src/Config/config.js
@@ -1,24 +1,37 @@
 import dotenv from "dotenv";
 dotenv.config();
 
-if(!process.env.PORT){
+if (!process.env.PORT) {
     throw new Error("PORT no is not defined");
 }
 
-if(!process.env.MONGO_URL){
+if (!process.env.MONGO_URL) {
     throw new Error("MONGO_URL is not defined");
 }
 
-
-if(!process.env.CLERK_WEBHOOK_KEY){
+if (!process.env.CLERK_WEBHOOK_KEY) {
     throw new Error("CLERK_WEBHOOK_KEY is not defined");
+}
+
+if (!process.env.CLOUDINARY_CLOUD_NAME) {
+    throw new Error("CLOUDINARY_CLOUD_NAME is not defined");
+}
+
+if (!process.env.CLOUDINARY_API_KEY) {
+    throw new Error("CLOUDINARY_API_KEY is not defined");
+}
+
+if (!process.env.CLOUDINARY_API_SECRET) {
+    throw new Error("CLOUDINARY_API_SECRET is not defined");
 }
 
 const config = {
     PORT: process.env.PORT,
-    MONGO_URL:process.env.MONGO_URL,
-    CLERK_WEBHOOK_KEY:process.env.CLERK_WEBHOOK_KEY,
-
+    MONGO_URL: process.env.MONGO_URL,
+    CLERK_WEBHOOK_KEY: process.env.CLERK_WEBHOOK_KEY,
+    CLOUDINARY_CLOUD_NAME: process.env.CLOUDINARY_CLOUD_NAME,
+    CLOUDINARY_API_KEY: process.env.CLOUDINARY_API_KEY,
+    CLOUDINARY_API_SECRET: process.env.CLOUDINARY_API_SECRET,
 }
 
 export default config;
